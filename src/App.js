fa6859c683f7297fa4d610e60ce5b8fde37e4288
APareja12 <alexis.pareja@hotmail.com>
2022-05-09 14:24:04 -0400

    adds settings pg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import Topbar from "./components/topbar/Topbar";
 // import Single from "./components/single/Single";
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 
 function App() {
   return (
     <>
     <Topbar/>
-    <Write/>
+    <Settings/>
     </>
   );
 }

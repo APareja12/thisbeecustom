ab9793c18e32f600d4cb9741f428091836cd12c7
APareja12 <alexis.pareja@hotmail.com>
2022-05-09 12:29:47 -0400

    adds write pg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Homepage from "./pages/homepage/Homepage";
+// import Homepage from "./pages/homepage/Homepage";
 import Topbar from "./components/topbar/Topbar";
+// import Single from "./components/single/Single";
+import Write from "./pages/write/Write";
+
 
 function App() {
   return (
     <>
     <Topbar/>
-    <Homepage/>
+    <Write/>
     </>
   );
 }
